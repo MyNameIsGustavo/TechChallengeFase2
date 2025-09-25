9c36f205a056d8b112ee968aab1a5df51ad9e828
MyNameIsGustavo <gustavo.professional.contact@gmail.com>
2025-09-25 20:21:47 -0300

    fix:: dotenv - secret key

--- src/middleware/autenticacao-middleware.ts
+++ src/middleware/autenticacao-middleware.ts
@@ -11,11 +11,12 @@ export function autenticacaoMiddleware(req: Request, res: Response, next: NextFu
     if (!token) return res.status(401).json({ mensagem: "Token de autenticação não fornecido" });
     
     try {
-        const secret = process.env.JWT_SECRET as string;
+        const secret = process.env.SECRET_KEY as string;
         const decoded = jwt.verify(token, secret);
         (req as any).usuario = decoded;
         next();
     } catch (err) {
+        console.error("Erro JWT:", err);
         return res.status(403).json({ mensagem: "Token inválido ou expirado" });
     }
 }
