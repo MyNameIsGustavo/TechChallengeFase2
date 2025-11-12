e31f90e77701773a050e8755bdf0362739802c95
MyNameIsGustavo <gustavo.professional.contact@gmail.com>
2025-11-11 21:02:18 -0300

    cors

--- src/servidor.ts
+++ src/servidor.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import dotenv from "dotenv";
+import cors from 'cors';
 
 dotenv.config();
 
 export const app = express();
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 const PORT = Number(process.env.API_PORT_DEV) || 3000;
