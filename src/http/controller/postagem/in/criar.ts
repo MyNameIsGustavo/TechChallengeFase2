c49b3653ef894e18add132b5591d44c4b63751da
MyNameIsGustavo <gustavo.professional.contact@gmail.com>
2025-12-27 19:17:23 -0300

    novo banco

--- src/http/controller/postagem/in/criar.ts
+++ src/http/controller/postagem/in/criar.ts
@@ -6,7 +6,7 @@ import dotenv from "dotenv";
 const envFile = process.env.NODE_ENV === "PRODUCTION" ? ".env.prod" : ".env.local";
 dotenv.config({ path: envFile });
 
-export async function criar(request: Request, response: Response) {
+export async function criar(request: Request, response: Response) {-
   try {
     const criarPostagemSchema = z.object({
       caminhoImagem: z.string().max(500).optional(),
